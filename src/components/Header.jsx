7450c5b35c11fe20e7be831d8c18674d28e66dbc
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-09 23:37:08 +0530

    add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <header className="bg-gray-900 text-white p-4">
-      <h1 className="text-xl font-bold">YT Clone</h1>
+    <header className="bg-gray-900 text-white p-4 flex justify-between items-center">
+      <Link to="/" className="text-xl font-bold">
+        YT Clone
+      </Link>
+      <nav className="space-x-4">
+        <Link to="/" className="hover:text-blue-400">
+          Home
+        </Link>
+        <Link to="/auth" className="hover:text-blue-400">
+          Sign In
+        </Link>
+      </nav>
     </header>
   );
 }
