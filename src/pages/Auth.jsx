f6a47aee75fcd1ca0505e5a134cc4f52924f8fab
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-09 23:39:24 +0530

    add Auth page

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,8 @@
+export default function Auth() {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-semibold">Authentication Page</h2>
+      <p className="text-gray-600">Sign up or log in to continue.</p>
+    </div>
+  );
+}
