2d2657d1a88abdca9c3a942be9f27dec13831406
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-09 23:32:09 +0530

    setup React Router with Home and VideoPlayer pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
+import VideoPlayer from "./pages/VideoPlayer";
+import Channel from "./pages/Channel";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex flex-1">
+          <Sidebar />
+          <main className="flex-1 p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/watch/:id" element={<VideoPlayer />} />
+              <Route path="/channel/:id" element={<Channel />} />
+              <Route path="/auth" element={<Auth />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
   );
 }
 
