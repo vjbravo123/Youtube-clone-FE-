835b4ef80a844485bb4dd6284ac50d3675c0130e
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-09 23:38:55 +0530

    add Channel page

---
+++ src/pages/Channel.jsx
@@ -0,0 +1,8 @@
+export default function Channel() {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-semibold">Channel Page</h2>
+      <p className="text-gray-600">Channel details and videos will appear here.</p>
+    </div>
+  );
+}
