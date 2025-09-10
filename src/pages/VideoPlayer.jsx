d01261f7bfd4b6836070f728ebf5cab81661c30a
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-10 08:11:26 +0530

    integrate CommentList into VideoPlayer

--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -1,8 +1,11 @@
+import CommentList from "../components/CommentList";
+
 export default function VideoPlayer() {
   return (
     <div className="p-4">
       <h2 className="text-lg font-semibold">Video Player</h2>
-      <p className="text-gray-600">Here the video will be played.</p>
+      <p className="text-gray-600 mb-4">Here the video will be played.</p>
+      <CommentList />
     </div>
   );
 }
