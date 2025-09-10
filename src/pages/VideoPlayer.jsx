fa91a57406a4bb0f6b1eb81db2158be800de4f39
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-10 07:57:53 +0530

    add Auth page

---
+++ src/pages/VideoPlayer.jsx
@@ -0,0 +1,8 @@
+export default function VideoPlayer() {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-semibold">Video Player</h2>
+      <p className="text-gray-600">Here the video will be played.</p>
+    </div>
+  );
+}
